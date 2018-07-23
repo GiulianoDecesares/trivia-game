9982d55fc035ad70a7bf444587adc162551f899b
GIuliano Decesares <Giulianodecesares@gmail.com>
2018-07-22 21:42:35 -0300

    Carousel behaviour updated and prefab manager bug fixed

--- Assets/Scripts/CarouselManager.cs
+++ Assets/Scripts/CarouselManager.cs
@@ -6,13 +6,33 @@ using UnityEngine.UI;
 public class CarouselManager : MonoBehaviour 
 {
     [SerializeField] private RectTransform contentRect;
+    [SerializeField] private RectTransform centerPlaceholderRect;
+    [SerializeField] private int questionCardAmount;
+    [SerializeField] private float intercardSpacing;
 
+    private void SetUpQuestionCards()
+    {
+        // Get the prefab to instance
+        GameObject questionCardPrefab = PrefabManager.instance.GetPrefabByName("QuestionCard");
+
+        // Build spacing vector using parameter from inspector
+        Vector2 spacing = new Vector2(this.intercardSpacing, 0f);
+
+        // Calculate offset taking the half width of a question card
+        Vector2 offset = new Vector2((float)(questionCardPrefab.GetComponent<RectTransform>().rect.width), 0f);
+
+        // Instantiation of prefabs 
+        for(int index = 0; index < this.questionCardAmount; index++)
+        {
+            Vector2 newPosition = (offset + spacing) * index; // Multiply position by index (items amount in content)
+            GameObject questionCardInstance = Instantiate(questionCardPrefab, this.contentRect); // Instantiation
+            questionCardInstance.GetComponent<RectTransform>().anchoredPosition = newPosition; // Set new position
+        }
+    }
 
     private void Start()
     {
-        GameObject prefab = PrefabManager.instance.GetPrefabByName("QuestionCard");
-
-        Instantiate(prefab, contentRect);
+        this.SetUpQuestionCards();        
     }
 
 
