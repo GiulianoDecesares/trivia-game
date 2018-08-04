9f944966d36e873b5873d86d941bed766de1745f
Mangangakid <mangangakid@gmail.com>
2018-08-03 21:15:45 -0300

    Option card script made and added to the prefab

---
+++ Assets/Scripts/OptionCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionCard : MonoBehaviour {
+    [HideInInspector]
+    public bool isCorrect;
+    public Text optionText;
+
+	// Use this for initialization
+	public void LoadOptionCard (string newOptionText, bool newIsCorrect)
+    {
+        isCorrect = newIsCorrect;
+        optionText.text = newOptionText;
+	}
+	
+}
