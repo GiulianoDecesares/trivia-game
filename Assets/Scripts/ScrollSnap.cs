c3249eba09a2c575cb162a7b3cf575c5c6b85d0b
GIuliano Decesares <Giulianodecesares@gmail.com>
2018-08-27 01:24:43 -0300

    Scroll snap improved behavior added

--- Assets/Scripts/ScrollSnap.cs
+++ Assets/Scripts/ScrollSnap.cs
@@ -5,64 +5,112 @@ using UnityEngine.UI;
 
 public class ScrollSnap : MonoBehaviour
 {
-    public ScrollRect screenScroll;
+    [SerializeField] private ScrollRect mainScroll;
+    [SerializeField] private RectTransform center;
 
-    [Range(0.1f, 0.5f)] public float animDelay = 0.2f;
+    [SerializeField] private List<Panel> allPanelsList;
 
-    public enum States { LoginPanel, PlayPanel, ResultPanel };
+    public float scrollSpeed;
 
-    public States screenState = States.LoginPanel;
+    public Panel.PanelType currentPanel = Panel.PanelType.RESULT;
 
     public System.Action onScrollEnd;
 
-    //This sets the screen state to Login Panel on start
-    void Start()
+    private void Start()
     {
-        screenState = States.LoginPanel;
+        this.mainScroll.normalizedPosition = new Vector2(0.98f, 0f);
+
+        StartCoroutine(SwipeTo(Panel.PanelType.LOGIN));
     }
 
-    //This method is called to change between screens using the state enum
-    public void ChangeScreen(States toScreen)
+    private Panel SearchForPanel(Panel.PanelType thisPanel)
     {
-        StartCoroutine(ScreenAnimation(toScreen));
-    }
+        Panel result = null;
 
-    //This method is called to change between screens using the state number
-    public void ChangeScreen(int toScreen)
-    {
-        switch (toScreen)
+        foreach(Panel panel in this.allPanelsList)
         {
-            case 0:
-                StartCoroutine(ScreenAnimation(States.LoginPanel));
+            if(panel.panelType == thisPanel)
+            {
+                result = panel;
                 break;
-            case 1:
-                StartCoroutine(ScreenAnimation(States.PlayPanel));
-                break;
-            case 2:
-                StartCoroutine(ScreenAnimation(States.ResultPanel));
-                break;
-            default:
-                Debug.Log("Out of Range");
-                break;
+            }
+        }
+
+        if(result == null)
+        {
+            Debug.LogError("Null panel returned");
+        }
+
+        return result;
+    }
+
+    private IEnumerator SwipeForoward(float normalizedScrollTo)
+    {
+        while(this.mainScroll.normalizedPosition.x < normalizedScrollTo)
+        {
+            Debug.Log("Main scroll position " + this.mainScroll.normalizedPosition);
+            Debug.Log("Normalized scrool to " + normalizedScrollTo);
+
+            mainScroll.normalizedPosition += new Vector2(scrollSpeed, 0) * Time.deltaTime;
+
+            yield return null;
         }
     }
 
-    //This is the corutine called by the ChangeScreen method to animate the transition between two screens
-    private IEnumerator ScreenAnimation(States newScreen)
+    private IEnumerator SwipeBack(float scrolltoSwipe)
     {
-        if (newScreen != screenState)
+        while(this.mainScroll.normalizedPosition.x >= (1 - scrolltoSwipe))
         {
-            float increment = ((int)newScreen - (int)screenState) / 40f;
+            Debug.Log("Main scroll position " + this.mainScroll.normalizedPosition);
+            Debug.Log("Normalized scrool to " + scrolltoSwipe);
 
-            for (int i = 0; i < 20; i++)
+            mainScroll.normalizedPosition -= new Vector2(scrollSpeed, 0) * Time.deltaTime;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator SwipeTo(Panel.PanelType thisPanel)
+    {
+        Panel targetPanel = this.SearchForPanel(thisPanel);
+        Debug.Log("Target panel type is " + targetPanel.panelType);
+
+        Panel currentPanel = this.SearchForPanel(this.currentPanel);
+        Debug.Log("Current panel type is " + currentPanel.panelType);
+
+        if(targetPanel)
+        {
+            int sibilingIndex = targetPanel.GetSibilingIndex();
+            Debug.Log("Target panel sibiling index : " + sibilingIndex);
+
+            int currentSibilingIndex = currentPanel.GetSibilingIndex();
+            Debug.Log("Current panel sibiling index : " + currentSibilingIndex);
+
+            float normalizedScrollTo = (1 / this.allPanelsList.Count) * sibilingIndex + 1;
+            Debug.Log("Normalized scroll to : " + normalizedScrollTo);
+
+            if(sibilingIndex < currentSibilingIndex)
             {
-                screenScroll.horizontalNormalizedPosition += increment;
-
-                yield return new WaitForSeconds(animDelay / 20f);
+                Debug.Log("Must swipe back");
+                StartCoroutine(this.SwipeBack(normalizedScrollTo));
+            }
+            else
+            {
+                Debug.Log("Must swipe foroward");
+                StartCoroutine(this.SwipeForoward(normalizedScrollTo));
             }
 
-            screenState = newScreen;
+
+            this.currentPanel = thisPanel;
         }
-            this.onScrollEnd?.Invoke();
+
+        this.onScrollEnd?.Invoke();
+
+        yield return null;
+    }
+
+    public void AnimatedSwipeTo(Panel.PanelType thisPanel)
+    {
+        StartCoroutine(this.SwipeTo(thisPanel));
     }
 }
